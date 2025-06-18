57fd0213e6c5db00854d7f665f15463fdc8f3846
crisstel07 <orozcocristel99@gmail.com>
2025-06-18 11:04:32 -0600

    Prueba

---
+++ src/Main/Principal.java
@@ -0,0 +1,38 @@
+
+package Main;
+
+import javax.swing.*;
+
+
+public class Principal extends JFrame{
+    
+    public Principal () {
+    //Toda la logica del JFrame
+    
+    }
+    
+    public void AgregarPaneles(){
+    // Agregar la logica de los paneles
+    }
+    
+    public void AgregarComponentes(){
+    //Todas las llamadas de los metodos que generan los componentes
+    }
+    
+    public void Label(){
+    // Todas las JLabel a utilizar
+    }
+    
+    public void Bottons(){
+    // Todos los JBottons a utilizar
+    }
+    
+    public void TextField(){
+    //Todos los JTextField a utilizar
+    
+    }
+    
+    
+}
+
+
